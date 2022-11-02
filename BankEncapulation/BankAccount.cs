e83570a429ac976c4fd4794e093a69c52dd8d45b
benjamin kunkel <benjamink1125@gmail.com>
2022-11-02 12:56:24 -0700

    innitial commit

---
+++ BankEncapulation/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace BankEncapulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        private double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance = amount;
+
+        }
+
+
+        public double GetBalance()
+        {
+            return $"{balance}";
+        }
+            }
+
+
+
+
+
+        
+    }
+}
+
---
+++ BankEncapulation/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BankEncapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+
+            Console.Writeline("how mush would you liek to deposit?");
+            var amountToDeposit = double.Parse(Console.Readline());
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you, youre blance is now {Account.GetBalance()}");
+            
+        }
+    }
+}
